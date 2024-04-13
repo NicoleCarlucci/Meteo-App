cfe317594491eabe84cd8dbef70dd494eb6450a3
nicole_c <nicole.carlucci92@gmail.com>
2024-04-13 16:12:43 +0200

    Fill in suggestion panel

--- js/main.js
+++ js/main.js
@@ -46,9 +46,36 @@ async function onSuccess(position) {
     weatherIcon.alt = description;
     weatherIcon.src = `./img/${iconCode}.png`;
     weatherTemperature.innerText = `${Math.floor(data.main.temp)}°`;
+    suggestion.innerText = getSuggestion(iconCode);
 
     //Disable loader
-    htmlElement.className = "";
-
-    
+    htmlElement.className = "";    
 }
+
+//Function to set appropried suggestion
+function getSuggestion(iconCode) {
+    const suggestions = {
+        '01d': 'Ricordati la crema solare!',
+        '01n': 'Buonanotte!',
+        '02d': 'Oggi il sole va e viene...',
+        '02n': 'Attenti ai lupi mannari...',
+        '03d': 'Luce perfetta per fare foto!',
+        '03n': 'Dormi sereno :)',
+        '04d': 'Che cielo grigio :(',
+        '04n': 'Non si vede nemmeno la luna!',
+        '09d': 'Prendi l\'ombrello',
+        '09n': 'Copriti bene!',
+        '10d': 'Prendi l\'ombrello',
+        '10n': 'Copriti bene!',
+        '11d': 'Attento ai fulmini!',
+        '11n': 'I lampi accendono la notte!',
+        '13d': 'Esci a fare un pupazzo di neve!',
+        '13n': 'Notte perfetta per stare sotto il piumone!',
+        '50d': 'Accendi i fendinebbia!',
+        '50n': 'Guida con prudenza!'
+      };
+
+      return suggestions[iconCode];
+}
+
+
